fd14115961e22ca1ea1b0836b2bb24a605092770
santyr <6dcea3ab-e73b-4cd2-8278-d949995d101f@bolverker.anonaddy.com>
2023-12-04 11:41:54 -0700

    get invoice keys from .env, misc fixes

--- fastapi_ap.py
+++ fastapi_ap.py
@@ -18,13 +18,14 @@ load_dotenv()
 
 ohauth1 = os.getenv('OH_AUTH_1')
 api_key = os.getenv('API_KEY')
-wallet_id = os.getenv('WALLET_ID')
-api_url = os.getenv('API_URL')
+herd_key = os.getenv('HERD_KEY')
+sat_key = os.getenv('SAT_KEY')
+api_url= os.getenv('API_URL')
 google_api_key = os.getenv('GOOGLE_API_KEY')
 nos_sec = os.getenv('NOS_SEC')
 
 # List all the environment variables that your application depends on
-required_env_vars = ['OH_AUTH_1', 'API_KEY', 'WALLET_ID', 'API_URL', 'GOOGLE_API_KEY', 'NOS_SEC']
+required_env_vars = ['OH_AUTH_1', 'API_KEY', 'HERD_KEY', 'SAT_KEY', 'API_URL', 'GOOGLE_API_KEY', 'NOS_SEC']
 
 # Check which environment variables are set
 missing_vars = [var for var in required_env_vars if os.getenv(var) is None]
@@ -110,38 +111,70 @@ async def convert_to_sats(client: httpx.AsyncClient, amount: float):
     except httpx.RequestError as e:
         logger.error(f'Failed to convert amount: {e}')
     return None
-
-async def send_lnurl_payment(client: httpx.AsyncClient):
+    
+async def create_invoice(client: httpx.AsyncClient, amount: int, memo: str, key: str = sat_key): # key is to wallet
     try:
-        url = 'https://lnb.bolverker.com/api/v1/payments/lnurl'
-        
-        # Convert to millisats, keep 2%
-        balance = await get_balance(client)
-        withdraw = round(balance * 0.98)
-        withdraw = round(withdraw / 1000) * 1000
+        url = "https://lnb.bolverker.com/api/v1/payments"
 
         headers = {
-            'accept': 'application/json',
-            'X-API-KEY': wallet_id,
-            'Content-Type': 'application/json'
+            "accept": "application/json",
+            "X-API-KEY": key,
+            "Content-Type": "application/json"
         }
 
         data = {
-            "description_hash": "dd9f408b0ebc0a7e55c47a4ada760ff4875e0d1692f9ef5761abccda129478cc",
-            "callback": "https://lnb.bolverker.com/lnurlp/api/v1/lnurl/cb/lnaddr/QzBo8z",
-            "amount": withdraw,
-            "comment": "⚡Lightning Goats⚡ Thank you for the treats!",
-            "description": "Lightning Goats - reset herd wallet"
+            "unit": "sat",
+            "internal": False,
+            "out": False,
+            "amount": amount,
+            "memo": memo,
         }
 
-        response = await client.post(url, headers=headers, json=data)  # Using the passed client instance
-        if response.status_code == 200:
-            return 200
+        response = await client.post(url, json=data, headers=headers)
+        
+        if response.status_code == 201:
+            response_data = response.json()
+            payment_request = response_data.get("payment_request")
+            if payment_request:
+                logger.info('Invoice created successfully.')
+                return payment_request
+            else:
+                logger.error('Payment request not found in the response.')
+                return None
         else:
-            logger.error(f'Failed to send payment, status code: {response.status_code}')
-            return response.status_code
+            logger.error(f'Failed to create invoice, status code: {response.status_code}')
+            return None
     except httpx.RequestError as e:
-        logger.error(f'Failed to send payment: {e}')
+        logger.error(f'Failed to create invoice: {e}')
+        return None
+        
+async def pay_invoice(client: httpx.AsyncClient, payment_request: str, key: str = herd_key):  # key is from wallet
+    try:
+        url = "https://lnb.bolverker.com/api/v1/payments"
+
+        headers = {
+            "accept": "application/json",
+            "X-API-KEY": key,
+            "Content-Type": "application/json"
+        }
+
+        data = {
+            "unit": "sat",
+            "internal": True,
+            "out": True,
+            "bolt11": payment_request
+        }
+
+        response = await client.post(url, json=data, headers=headers)
+        
+        if response.status_code == 201:
+            logger.info('Invoice payment successful.')
+            return 201
+        else:
+            logger.error(f'Failed to pay invoice, status code: {response.status_code}')
+            return None
+    except httpx.RequestError as e:
+        logger.error(f'Failed to pay invoice: {e}')
         return None
 
 async def remove_payment_hash_after_delay(payment_hash: str, delay: int):
@@ -176,8 +209,15 @@ async def trigger_feeder(client: httpx.AsyncClient):
 
 async def send_payment(amount: float, difference: float, event: str):
     for retry in range(3):
-        payment_status = await send_lnurl_payment(client)
-        if payment_status == 200:
+         # Convert to millisats, keep 10%
+        balance = await get_balance(client)
+        withdraw = int(round(balance * 0.98) / 1000)
+        memo = 'Reset Herd Wallet'
+
+        payment_request = await create_invoice(client, withdraw, memo)
+        payment_status = await pay_invoice(client, payment_request)
+
+        if payment_status == 201:
             await run_nostril_command(nos_sec, amount, difference, event)
             break
         else:
@@ -203,7 +243,7 @@ async def webhook(data: HookData):
         return 'feeder_off'
     
     description = data.description
-    amount = data.amount / 1000 if data.amount else 0
+    amount = int(data.amount / 1000) if data.amount else 0
     balance = float(await get_balance(client)) / 1000
     trigger = float(await convert_to_sats(client, 1))  # dollar amount - goal to reach for triggering feeder
 
@@ -233,6 +273,7 @@ async def convert(amount: float):
     sats = await convert_to_sats(client, amount)
     if sats is not None:
         return sats
+
 
 @app.get("/islive/{channel_id}")
 async def live_status(channel_id: str):
