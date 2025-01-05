2b493961234a7a67a00374a247c86bb0d5e6ca23
Sat <792024+santyr@users.noreply.github.com>
2025-01-05 12:21:27 -0700

    Update messaging.py

--- messaging.py
+++ messaging.py
@@ -57,7 +57,9 @@ async def make_messages(
     goat_names_dict = {
         "Dexter": ["nostr:nprofile1qqsw4zlzyfx43mc88psnlse8sywpfl45kuap9dy05yzkepkvu6ca5wg7qyak5", "ea8be2224d58ef0738613fc327811c14feb4b73a12b48fa1056c86cce6b1da39"],
         "Rowan": ["nostr:nprofile1qqs2w94r0fs29gepzfn5zuaupn969gu3fstj3gq8kvw3cvx9fnxmaugwur22r", "a716a37a60a2a32112674173bc0ccba2a3914c1728a007b31d1c30c54ccdbef1"],
-        # Add other goats here
+        "Nova": ["nostr:nprofile1qqsrzy7clymq5xwcfhh0dfz6zfe7h63k8r0j8yr49mxu6as4yv2084s0vf035", "3113d8f9360a19d84deef6a45a1273ebea3638df2390752ecdcd76152314f3d6"],
+        "Cosmo": ["nostr:nprofile1qqsq6n8u7dzrnhhy7xy78k2ee7e4wxlgrkm5g2rgjl3napr9q54n4ncvkqcsj", "0d4cfcf34439dee4f189e3d959cfb3571be81db744286897e33e8465052b3acf"],
+        "Newton": ["nostr:nprofile1qqszdsnpyzwhjcqads3hwfywt5jfmy85jvx8yup06yq0klrh93ldjxc26lmyx", "26c261209d79601d6c2377248e5d249d90f4930c72702fd100fb7c772c7ed91b"]
     }
 
     message_dict = {
